713ac9478a302b1c16d9c60fded9572ff702f813
jbear18 <league@WorthyMini-6.attlocal.net>
2017-09-08 16:59:50 -0700

    Level 0 Skills

--- src/SkillPractice.java
+++ src/SkillPractice.java
@@ -1,3 +1,6 @@
+import java.net.URI;
+
+import javax.swing.JOptionPane;
 
 public class SkillPractice {
 public static void main(String[] args) {
@@ -9,18 +12,37 @@ public static void main(String[] args) {
 	skills.skill4();
 	skills.skill5();
 }
+static void playVideo(String videoURL) {
+	try {
+		URI uri = new URI(videoURL);
+		java.awt.Desktop.getDesktop().browse(uri);
+	} catch (Exception e) {
+		e.printStackTrace();
+	}
+}
 void skill1() {
 	// Use pop-ups for the following. DO NOT use Eclipse short cuts to help you
 	// Ask the user how many dimes they have
-	
+	String ans=JOptionPane.showInputDialog("How many dimes do you have?");
+	int ansNumber=Integer.parseInt(ans);
 	// Tell them how many cents they have (hint multiply by 10)
+	JOptionPane.showMessageDialog(null, "You have"+ansNumber*10 +"cents");
 	
 	// Ask the user how tall they are (inches)
-	
+	String tall=JOptionPane.showInputDialog("How tall are you?");
+	int tallNumber= Integer.parseInt(tall);
 	// If they are shorter than 36 inches, tell them to eat their Wheaties
+	if(tallNumber<26){
+	
+		
+}
+	else {
+	JOptionPane.showMessageDialog(null, "Eat your WHEATIES!!");
+	playVideo("https://www.youtube.com/watch?v=BmBh0NNEm00");
+
+	}
 	
 }
-
 void skill2() {
 	// Write a loop to print every third number between 1 and 30 to the console
 	// Do NOT use Eclipse short-cuts to help you
